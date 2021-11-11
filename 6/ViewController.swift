0f5a2acd88541e792cfb70a0690bf6f4ed36c172
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-11 10:58:02 +0900

    update based on feedback

--- 6/ViewController.swift
+++ 6/ViewController.swift
@@ -11,33 +11,43 @@ class ViewController: UIViewController {
     @IBOutlet private weak var targetNumLabel: UILabel!
     @IBOutlet private weak var selectNumSlider: UISlider!
 
+    private var answer = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        targetNumLabel.text = String(Int.random(in: 1...100))
-        selectNumSlider.value = 50
+        resetGames()
     }
 
     @IBAction private func checkButtonPressed(_ sender: UIButton) {
-        let targetedNum = Int(targetNumLabel.text!)
         let selectedNum = Int(selectNumSlider.value)
         let resultText: String
 
-        if targetedNum != selectedNum {
-            resultText = "はずれ！"
-        } else {
+        if answer == selectedNum {
             resultText = "あたり！"
+        } else {
+            resultText = "はずれ！"
         }
         let alert = UIAlertController(title: "結果",
                                       message: "\(resultText)\nあなたの値：\(selectedNum)",
                                       preferredStyle: .alert)
 
-        let cancelAction = UIAlertAction(title: "再挑戦", style: .cancel) { _ in
-            self.targetNumLabel.text = String(Int.random(in: 1...100))
-            self.selectNumSlider.value = 50
+        let cancelAction = UIAlertAction(title: "再挑戦", style: .cancel) { [weak self] _ in
+            // 今回の場合、weak selfをつけなくてもメモリリークは起きない。
+            // ただ、一般的にこのクロージャをプロパティにしたときにはメモリリークが発生してしまうので、安全面を考えて、
+            // selfを参照するクロージャにはとりあえず、[weak self]をつけた方が良い（by あきおさん）
+
+            // selfの場合、unownedの方が良いのでは？
+            self?.resetGames()
         }
 
         alert.addAction(cancelAction)
         present(alert, animated: true)
+    }
+
+    private func resetGames() {
+        answer = Int.random(in: 1...100)
+        targetNumLabel.text = String(answer)
+        selectNumSlider.value = 50
     }
 }
